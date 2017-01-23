1c33385f8dfa59f71fa343d3270273e061a94bd7
mattvox <vox-mini@Matthews-Mini.home>
2017-01-22 21:46:34 -0500

    Initial commit.

---
+++ public/main.js
@@ -0,0 +1,47 @@
+var socket = io();
+
+var pictionary = function() {
+    var canvas, context;
+
+    var draw = function(position) {
+        context.beginPath();
+        context.arc(position.x, position.y,
+                         6, 0, 2 * Math.PI);
+        context.fill();
+    };
+
+    canvas = $('canvas');
+    context = canvas[0].getContext('2d');
+    canvas[0].width = canvas[0].offsetWidth;
+    canvas[0].height = canvas[0].offsetHeight;
+    
+    var drawing = false;
+    
+    canvas.on('mousedown', function() {
+        drawing = true;
+    })
+    
+    canvas.on('mouseup', function() {
+        drawing = false;
+    })
+    
+    canvas.on('mousemove', function(event) {
+        if (drawing === true) {
+            var offset = canvas.offset();
+            var position = {x: event.pageX - offset.left,
+                            y: event.pageY - offset.top};
+
+            draw(position);
+            socket.emit('draw', position);
+            
+            
+        } 
+    });
+    
+    socket.on('draw', draw);
+};
+
+$(document).ready(function() {
+    pictionary();
+});
+
